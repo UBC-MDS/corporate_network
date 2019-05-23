332a8627d378ea3f13ee3f64185a625a00d5d11a
Daniel Lin <dglin@misr.sauder.ubc.ca>
2019-05-23 11:51:52 -0700

    updated dropdown to populate based on available files

---
+++ get_years.php
@@ -0,0 +1,74 @@
+<?php
+// insert();
+
+// function select() {
+//     echo "The select function is called.";
+//     exit;
+// }
+
+// function insert() {
+//     echo "The insert function is called.";
+//     exit;
+// }
+
+// $dirs = array_filter(glob('*'), 'is_dir');
+// print_r($dirs);
+
+function getDirContents($dir, &$results = array()){
+    $files = scandir($dir);
+
+    foreach($files as $key => $value){
+        $path = realpath($dir.DIRECTORY_SEPARATOR.$value);
+        if(!is_dir($path)) {
+            $results[] = $path;
+        } else if($value != "." && $value != "..") {
+            getDirContents($path, $results);
+            $results[] = $path;
+        }
+    }
+
+    return $results;
+}
+
+function getAllDirs($dir, &$results = array()) {
+    $files = scandir($dir);
+
+    foreach($files as $key => $value){
+        $path = realpath($dir.DIRECTORY_SEPARATOR.$value);
+        if(is_dir($path) && $value != "." && $value != "..") {
+            $results[] = $path;
+        } 
+    }
+
+    return $results;
+}
+
+function getValidYears($dirs, $company, &$results = array()) {
+    foreach($dirs as $value) {
+        $arr_filepath = explode("/", $value);
+        $year = array_values(array_slice($arr_filepath, -1))[0];
+        // var_dump($value);
+        // var_dump($year);
+        $test_path = $value . '/' . $company . '.json';
+        $test_cited_path = $value . '/cited/' . $company . '.json';
+        $test_cited_path = $value . '/filed/' . $company . '.json';
+        // var_dump($test_path);
+        if (file_exists($test_path)) {            
+            $results[] = $year;
+        }
+    }
+
+    return $results;
+}
+
+if (isset($_POST['company'])) {
+    $comp_name = $_POST['company'];
+}
+
+$files = getDirContents('/var/www/html/corporate_network/json.full/converted');
+$dirs = getAllDirs('/var/www/html/corporate_network/json.full/converted');
+// var_dump($years);
+$years = getValidYears($dirs, $comp_name);
+// var_dump($files);
+echo json_encode($years);
+?>
